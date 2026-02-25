317de00bede32935a0279bb55549100dde4248e4
Cole Stuedeman <cstuedeman@gmail.com>
2026-02-24 20:13:00 -0600

    Finished

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/netflix-clone/"
+  base: "/netflix-clone/",
+  server: {
+    fs: {
+      strict: false
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
+  }
 })
